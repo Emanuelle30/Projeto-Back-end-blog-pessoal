a923ae80d3273d5018688c8b331c57084d5bf209
Emanuelle Cavalcante <emanuelle.cavalcante4@gmail.com>
2022-05-02 21:35:12 -0300

    initial commit

--- src/main/java/com/blogpessoal/blogpessoal/model/Postagem.java
+++ src/main/java/com/blogpessoal/blogpessoal/model/Postagem.java
@@ -6,11 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_postagens")
@@ -33,6 +36,18 @@ public class Postagem {
 	
 	private String foto;
 	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
+	
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 
 	public long getId() {
 		return id;
